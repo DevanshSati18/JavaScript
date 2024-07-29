73bf05b08c8a331f721bec6467cd9bb7db80681f
devanshsati1804 <90976960+devanshsati18@users.noreply.github.com>
2024-07-29 14:19:42 +0530

    update

---
+++ 4)AsyncronusJavascript/CallbackFunction.js
@@ -0,0 +1,2 @@
+//Callback Functions: Callback functions are functions passed as arguments to other functions and are executed after some operation completes. They are often used to handle asynchronous operations, such as making network requests or reading files.//
+
